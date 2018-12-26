ac204201e94dda354653f0a660c9e24ab0295aae
ederson badeca <edersonbadeca@gmail.com>
2018-12-26 13:41:56 -0200

    Fixes typos in classes

--- books/tasks.py
+++ books/tasks.py
@@ -5,18 +5,17 @@ from dateutil.relativedelta import relativedelta
 from django.core.mail import send_mail
 from django.conf import settings
 
-from .models import Book, Library, BookCopy
+from books.models import Book, Library, BookCopy
 
 
 @task()
 def send_borrows_out_of_time_notifications():
-    from celery.contrib import rbd;rdb.set_trace()
     borrow_book_out_of_time = get_borrows_out_of_time(settings.CRON_EMAIL_NOTIFICATION_SETTINGS["BORROW_MAX_TERM_MONTH"])
     send_notification(borrow_book_out_of_time)
 
 
 @task()
-def send_notification(self, book_list):
+def send_notification(book_list):
     for book_copy in book_list:
         send_mail(
             settings.CRON_EMAIL_NOTIFICATION_SETTINGS["TEMPLATE_SUBJECT"],
@@ -28,6 +27,6 @@ def send_notification(self, book_list):
 
 
 @task()
-def get_borrows_out_of_time(self, number_months_borrowed_limit):
+def get_borrows_out_of_time(number_months_borrowed_limit):
     limitDate = datetime.datetime.today() - relativedelta(months=number_months_borrowed_limit)
     return list(BookCopy.objects.filter(borrow_date__lte=limitDate))

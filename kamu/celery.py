dc1609251ce087d87f9ed822de8438085d5fc096
ederson badeca <edersonbadeca@gmail.com>
2018-12-26 13:43:14 -0200

    Uses auto discovery tasks in the whole application

--- kamu/celery.py
+++ kamu/celery.py
@@ -4,19 +4,18 @@ import os
 from celery import Celery
 
 from django.conf import settings
-from books import tasks
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kamu.settings')
 app = Celery('kamu')
 
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(0.5, tasks.send_borrows_out_of_time_notification, name='add every 10')
+    sender.add_periodic_task(60, tasks.send_borrows_out_of_time_notifications, name='add every 10')
 
 
 @app.task(bind=True)
@@ -26,8 +25,8 @@ def debug_task(self):
 
 app.conf.beat_schedule = {
     'add-every-30-seconds': {
-        'task': 'books.tasks.send_borrows_out_of_time_notification',
-        'schedule': 0.5
+        'task': 'books.tasks.send_borrows_out_of_time_notifications',
+        'schedule': 60
     },
 }
 app.conf.timezone = 'UTC'
